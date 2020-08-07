b7dbbf6ae2c063fd15cb753b5546b7ccf0401b9c
Aleksandar Stefanovic <a.stefanovic2112@gmail.com>
2020-08-07 22:59:24 +0200

    Blog - added layout and 2 htmls

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse 
 
 # Create your models here.
 
@@ -30,3 +31,6 @@ class Post(models.Model):
 
     objects = models.Manager()
     published = PublishedManager()
+
+    def get_absolute_urls(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
