91cbe461e60508879c5c13d6245e79e96a894333
Aleksandar Stefanovic <a.stefanovic2112@gmail.com>
2020-08-08 16:19:42 +0200

    blog working now

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -32,5 +32,5 @@ class Post(models.Model):
     objects = models.Manager()
     published = PublishedManager()
 
-    def get_absolute_urls(self):
+    def get_absolute_url(self):
         return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def post_list(request):
@@ -9,7 +10,7 @@ def post_list(request):
     })
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, slug = post, status='published',publish_year = year, publish_month = month, publish_day= day)
-    return render(request, 'blog/post.detail.html',{
+    post = get_object_or_404(Post, slug = post, status='published',publish__year = year, publish__month = month, publish__day= day)
+    return render(request, 'blog/post/detail.html',{
         'post':post
     })
