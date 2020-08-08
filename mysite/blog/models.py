a3052527eacd0467de4a8799f83b7866e15d8f52
Aleksandar Stefanovic <a.stefanovic2112@gmail.com>
2020-08-08 18:23:17 +0200

    Chapter 1 done, blog working

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -7,7 +7,7 @@ from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return super(PublishedManager,self).get_queryset
+        return super(PublishedManager,self).get_queryset().filter(status='published')
 
 class Post(models.Model):
     STATUS_CHOICES = (
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator, EmptyPage,\
-                                  PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
 from .models import Post
+from django.views.generic import ListView
 
 # Create your views here.
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
 def post_list(request):
     object_list = Post.published.all()
     paginator = Paginator(object_list, 3) # 3 posts in each page
-    page = request.GET.get('page')
+    page = request.GET.get('page',1)
     try:
         posts = paginator.page(page)
     except PageNotAnInteger:
