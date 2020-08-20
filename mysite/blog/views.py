63fcb93db750f7f16b946775e0b02aaf51a377b6
Aleksandar Stefanovic <a.stefanovic2112@gmail.com>
2020-08-20 22:23:48 +0200

    added Simmilar Posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,6 +5,7 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from taggit.models import Tag
+from django.db.models import Count 
 
 # Create your views here.
 class PostListView(ListView):
@@ -54,11 +55,16 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+    # List of similar posts 
+    post_tags_ids = post.tags.values_list('id',flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
     return render(request, 'blog/post/detail.html',{
         'post':post,
         'comments': comments,
         'new_comment': new_comment,
-        'comment_form':comment_form
+        'comment_form': comment_form,
+        'similar_posts': similar_posts
     })
 
 def post_share(request, post_id):
