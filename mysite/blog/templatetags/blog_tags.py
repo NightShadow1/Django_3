26ffb2615dc45b46ad2f380862468cb95791cb97
Aleksandar Stefanovic <a.stefanovic2112@gmail.com>
2020-09-02 23:48:25 +0200

    added simple_tags and inclusion_tags

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from ..models import Post 
+from django.db.models import Count 
 
 register = template.Library()
 
@@ -7,3 +8,11 @@ register = template.Library()
 def total_posts():
     return Post.published.count()
 
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts':latest_posts}
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
