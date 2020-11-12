78d92076f2610a08aee692e5658619c59401fc86
Aleksandar Stefanovic <a.stefanovic2112@gmail.com>
2020-11-12 22:27:55 +0100

    sitemap.xml added

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+SITE_ID = 1
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
