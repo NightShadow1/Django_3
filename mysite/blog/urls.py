c0f28f02fe0594e7ff51fcc967f7645dba42e4bd
Aleksandar Stefanovic <a.stefanovic2112@gmail.com>
2020-12-04 21:25:47 +0100

    added RSS subscription

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path 
 from . import views
+from .feeds import LatestPostsFeed
 
 app_name = 'blog'
 
@@ -11,4 +12,5 @@ urlpatterns = [
     path('tag/<slug:tag_slug>/',views.post_list, name='post_list_by_tag'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',views.post_detail, name = 'post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
